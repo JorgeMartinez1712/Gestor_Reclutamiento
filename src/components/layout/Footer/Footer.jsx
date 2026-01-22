5ff49cb1f4166c9d77a335c3734af191e866fc90
Jorge Martinez <jotadaniel93@gmail.com>
2026-01-22 09:11:24 -0400

    Ajustes

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = ({ isSidebarOpen, isCollapsed }) => {
   return (
     <footer
-      className={`bg-gray-100 shadow-sm border-t border-gray-300 transition-all duration-300 ${
+      className={`bg-bg  transition-all duration-300 ${
         isSidebarOpen && !isCollapsed ? 'ml-64' : isCollapsed ? 'ml-16' : 'ml-0'
       }`}
     >
       <div className="container mx-auto p-3 flex justify-end items-center">
         <div className="text-right">
-          <p className="text-sm text-gray-700">
+          <p className="text-sm text-oscuro">
             © {new Date().getFullYear()} Administrador. Todos los derechos reservados.
           </p>
         </div>
